b4275642715ae96c545907b18cb48960b1d070df
Daniel Waardal <waawal@boom.ws>
2013-10-06 11:41:15 +0200

    fixing port

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def index():
 
 @app.route("/")
 def person(name):
-    ['<a href="/feed"> Feed </a>']
+    return flask.Response(['<a href="/feed"> Feed </a>'])
 
 @app.route("/unsubscribe/<token>")
 def unsubscribe(token):
